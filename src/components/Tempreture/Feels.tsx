6ae2077e2b1955051d39deeecf89a928ba38b6fb
George <alximik15@gmail.com>
2023-07-21 17:11:09 +0400

    Feels finished

--- src/components/Tempreture/Feels.tsx
+++ src/components/Tempreture/Feels.tsx
@@ -3,29 +3,43 @@ import { WeatherCard as MyWeatherCard } from "../WeatherCard";
 import { useForecastData } from '../../hooks';
 
 export const Feels = () => {
-    const { forecastData, isLoading, error } = useForecastData();
+  const { forecastData, isLoading, error } = useForecastData();
 
-    if (isLoading) {
-        return <p>Loading...</p>;
-      }
-    
-      if (error) {
-        return <p>{error}</p>;
-      }
-    
-      if (!forecastData) {
-        return <p>No forecast data available.</p>;
-      }
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
-   
-      
-      
-      const sunRise = forecastData.forecast.forecastday[0].astro.sunrise
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!forecastData) {
+    return <p>No forecast data available.</p>;
+  }
+
+  const feels = forecastData.current.feelslike_c;
+  let feelsDescription = "";
+
+  // Add your temperature conditions and descriptions here
+  if (feels < 5) {
+    feelsDescription = "Feels very cold";
+  } else if (feels < 10) {
+    feelsDescription = "Feels cold";
+  } else if (feels < 20) {
+    feelsDescription = "Feels cool";
+  } else if (feels < 25) {
+    feelsDescription = "Feels warm";
+  } else {
+    feelsDescription = "Feels hot";
+  }
 
   return (
     <MyWeatherCard>
-      <h5 className="mb-2 text-2xl font-bold flex justify-center text-gray-900"><Feel /></h5>
-      <p className="font-normal text-gray-700">{sunRise}</p>
+      <h5 className="mb-2  font-bold flex justify-center items-baseline text-gray-900">
+        <Feel /> Feels Like
+      </h5>
+      <p className="font-normal text-gray-700">{feels}°</p>
+      <p className="font-normal text-gray-700">{feelsDescription}</p>
     </MyWeatherCard>
   );
 };
